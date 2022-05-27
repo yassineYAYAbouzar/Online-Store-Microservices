f63deb136e13cfc24c43b9210f8f51b2b6d728ee
yassineYAYAbouzar <barberhb6@gmail.com>
2022-05-27 21:25:11 +0100

    add product service

---
+++ src/test/java/com/online/store/onlinestore/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.store.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
